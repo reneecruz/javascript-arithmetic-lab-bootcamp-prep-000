04121d651f200a74250bd9b26bcfb27402c6e3dc
Renée Cruz <reneebasscruz@gmail.com>
2019-01-24 04:37:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b) {
+  console.log(a + b);
+}
+
+function subtract(a,b) {
+  console.log(a - b);
+}
+
+
+function multiply(a,b) {
+  console.log(a * b);
+}
+
+function divide(a,b) {
+  console.log(a / b);
+}
+
+function inc(n) {
+  console.log(n += n);
+}
+
+function dec(n) {
+  console.log(n -= n);
+}
+
+function makeInt(n) {
+  parse.Int(n, 10);
+}
+
