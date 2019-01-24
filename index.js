b214cb776b2b14961fd3632fe16ed934aafada7a
Renée Cruz <reneebasscruz@gmail.com>
2019-01-24 04:49:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a,b) {
-  console.log(a + b);
+  return (a + b);
 }
 
 function subtract(a,b) {
@@ -8,11 +8,11 @@ function subtract(a,b) {
 
 
 function multiply(a,b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 function divide(a,b) {
-  console.log(a / b);
+  return (a / b);
 }
 
 function inc(n) {
